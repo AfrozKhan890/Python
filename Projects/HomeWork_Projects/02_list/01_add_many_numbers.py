ad0c7d878fb7327fd388351c9696f65c55e818b9
Afroz Khan <Afrozaftab11@gmail.com>
2025-04-12 12:02:01 +0500

    Add multiple numbers at one time.

---
+++ Projects/HomeWork_Projects/02_list/01_add_many_numbers.py
@@ -0,0 +1,10 @@
+# Write a function that takes a list of numbers and returns the sum of those numbers.
+
+#  function banaya
+def sum_of_numbers(numbers):
+    return sum(numbers)
+
+# List banaya add krny k lie 
+my_list = [1, 2, 3, 4, 5,6,7,8,9,10]
+total = sum_of_numbers(my_list)
+print("Sum:", total)
