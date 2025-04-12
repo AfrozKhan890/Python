d3f23a8d6608caac68cd0a6482dbbf870e22550f
Afroz Khan <Afrozaftab11@gmail.com>
2025-04-12 12:03:22 +0500

    List values multiply with 2.

---
+++ Projects/HomeWork_Projects/02_list/02_double_list.py
@@ -0,0 +1,15 @@
+# Write a program that doubles each element in a list of numbers. For example, if you start with this list:
+# numbers = [1, 2, 3, 4]
+# You should end with this list:
+# numbers = [2, 4, 6, 8]
+
+
+
+def double_elements(numbers):
+    for i in range(len(numbers)):
+        numbers[i] *= 2
+    return numbers
+
+
+numbers = [1, 2, 3, 4]
+print(f"List is doubled: {double_elements(numbers)}")
