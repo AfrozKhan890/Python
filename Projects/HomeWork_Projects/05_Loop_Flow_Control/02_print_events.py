19c191728b1e6ee3671d95f345504ef4f69ab279
Afroz Khan <Afrozaftab11@gmail.com>
2025-04-15 16:21:00 +0500

    This is already done.

---
+++ Projects/HomeWork_Projects/05_Loop_Flow_Control/02_print_events.py
@@ -0,0 +1,9 @@
+"""
+Write a program that prints the first 20 even numbers. There are several correct approaches, but they all use a loop of some sort. Do no write twenty print statements
+The first even number is 0:
+0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38
+"""
+
+
+# print_Events is already done 
+# GITHUB URL is "https://github.com/AfrozKhan890/Python/blob/main/Projects/HomeWork_Projects/03_If_statement/01_print_events.py"
